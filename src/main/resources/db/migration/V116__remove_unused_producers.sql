0629725764a363a614fca90a592dbfc68b396f6c
oKukHtina <olakukhtina95@gmail.com>
2026-06-21 22:46:51 +0300

    deleted unused regions

---
+++ src/main/resources/db/migration/V116__remove_unused_producers.sql
@@ -0,0 +1,11 @@
+DELETE FROM producers
+WHERE name IN (
+    'Dom Pérignon',
+    'Louis Roederer',
+    'Veuve Clicquot',
+    'Gérard Bertrand',
+    'Yellow Tail',
+    'Château d''Esclans',
+    'Miraval',
+    'Ornellaia'
+);
