8285363d1dc97bf7639f7c68c573701f2c276c23
oKukHtina <olakukhtina95@gmail.com>
2026-08-23 23:52:59 +0300

    fix icons

---
+++ src/main/resources/db/migration/V138__fix_icons.sql
@@ -0,0 +1,7 @@
+UPDATE achievements
+SET icon_url = 'https://res.cloudinary.com/dtnzeg5vh/image/upload/v1787517292/2026-08-23_23.34.16_ih9prt.jpg'
+WHERE code = 'FIRST_FAVORITE';
+
+UPDATE wines
+SET image_url = 'https://res.cloudinary.com/dtnzeg5vh/image/upload/v1784409089/Martini_Rosato_uthazo.png'
+WHERE name = 'Martini Rosato';
